6d5a8a0600519b6b05c45ed81cc683fb2a1da505
Allyson <79607941+Sachty@users.noreply.github.com>
2024-01-10 22:29:49 -0500

    forward casi terminado

--- main.cpp
+++ main.cpp
@@ -1,17 +1,3 @@
 //
 // Created by allxs on 10/01/2024.
 //
-
-#include <iostream>
-using namespace std;
-#include "double_list.h"
-
-int main() {
-    double_list* doubleList = new double_list;
-
-    // Agregando
-    doubleList->push_back(1);
-
-
-
-}
